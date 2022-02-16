6f21fd64d89cde808830d6dd8a59e39186f27a52
Lori Garzio <lgarzio@marine.rutgers.edu>
2022-02-16 13:09:49 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# gliderqcplot
+Scripts for plotting QC'd glider data.
+
+## Installation
+
+`git clone https://github.com/rucool/gliderqcplot.git`
+
+`cd gliderqcplot`
+
+`conda env create -f environment.yml`
+
+`conda activate gliderqcplot`
+
+`pip install .`
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gliderqcplot',
+    packages=find_packages(),
+    url='https://github.com/rucool/gliderqcplot',
+    author='Lori Garzio',
+    author_email='lgarzio@marine.rutgers.edu',
+    description="Scripts for plotting QC'd glider data."
+)
